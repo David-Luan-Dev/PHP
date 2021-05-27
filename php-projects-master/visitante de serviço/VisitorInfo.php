a48d70fe48326a5fa0041710d79787d55286be02
David Luan <davidnunes.14@hotmail.com>
2021-05-26 22:59:14 -0300

    Update VisitorInfo.php

--- php-projects-master/visitante de serviço/VisitorInfo.php
+++ php-projects-master/visitante de serviço/VisitorInfo.php
@@ -2,8 +2,7 @@
 /* ---------------------------------------------------------------------------- 
  *  Visitor Info Service (v.1.5.1)
  * ---------------------------------------------------------------------------- 
- *  Copyright (C) 2021 Robson S. Martins
- *  Robson Martins <https://www.robsonmartins.com>
+ * byL.David  
  * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
